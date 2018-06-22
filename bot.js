ed1aa2d4534fd6abc7c381a20064d0f86c9106d1
MMay1818Smh <40446817+MMay1818Smh@users.noreply.github.com>
2018-06-22 18:34:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,14 +6,23 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'Trainings') {
+    if (message.content === '-shift') {
     	message.channel.send('@everyone Shift! Come on down to the cafe for a possible promotion! https://www.roblox.com/games/1835287884/May-Cafe');
   	}
 });
-
 client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
+    if (message.content === '-interviews') {
+    	message.channel.send('@everyone Interviews! Interviews are starting in 15 minutes! Hurry and join! https://www.roblox.com/games/1887056994/May-Cafe-Interview-Centere');
+    }
+});
+client.on('message', message => {
+    if (message.content === '-trainings') {
+    	message.channel.send('@everyone Trainings! Trainings are starting in 15 minutes! Hurry and join! https://www.roblox.com/games/1890506534/May-Cafes-GRAND-OPENING-Trainings');
+  	}
+});
+client.on('message', message => {
+    if (message.content === '-ban') {
+    	message.reply('You will now be banned for banning someone else!');
   	}
 });
 
