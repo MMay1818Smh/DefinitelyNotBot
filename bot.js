8e9c4a67acd2f57de9e48ef9bab237fc422d1bd1
MMay1818Smh <40446817+MMay1818Smh@users.noreply.github.com>
2019-02-02 23:47:10 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,16 +7,12 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!show') {
-    	message.channel.send('@everyone ` There is a show going on in 15 minutes! Come on down to the theatre to watch! Link:link: : https://www.roblox.com/games/2683021995/Henry-Theatre-Theatre `
-                             
-                             ***Scripted by MMay1818***');
+    	message.channel.send('@everyone ` There is a show going on in 15 minutes! Come on down to the theatre to watch! Link:link: : https://www.roblox.com/games/2683021995/Henry-Theatre-Theatre ` ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === '!auditions') {
-    	message.channel.send('@everyone `Auditions are happening in 15 minutes. These are hosted by @Management and up. Come on down to get an actor position! https://www.roblox.com/games/2744510875/Auditions `
-                             
-                             ***Scripted by MMay1818***');
+    	message.channel.send('@everyone `Auditions are happening in 15 minutes. These are hosted by @Management and up. Come on down to get an actor position! https://www.roblox.com/games/2744510875/Auditions ` ***Scripted by MMay1818***');
     }
 });
 client.on('message', message => {
@@ -26,103 +22,72 @@ client.on('message', message => {
 });
 client.on('message', message => {
     if (message.content === 'fuck') {
-    	message.reply('Do not swear in this discord server!
-                      
-                      ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === 'Fuck') {
-    	message.reply('Do not swear in this discord server!
-                      
-                      ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === 'bitch') {
-    	message.reply('Do not swear in this discord server!
-                      
-                      ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === 'Bitch') {
-    	message.reply('Do not swear in this discord server!
-                      
-                      ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === 'cunt') {
-    	message.reply('Do not swear in this discord server!
-                      
-                      ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === 'Cunt') {
-    	message.reply('Do not swear in this discord server!
-                      
-                      ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === 'asshole') {
-    	message.reply('Do not swear in this discord server!
-                      
-                      ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === 'Asshole') {
-    	message.reply('Do not swear in this discord server!
-                      
-                      ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === 'ass') {
-    	message.reply('Do not swear in this discord server!
-                      
-                      ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === 'Ass') {
-    	message.reply('Do not swear in this discord server!
-                      
-                      ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === '!close') {
-    	message.channel.send('@everyone This chat is now closed. Talking ***WILL*** get you banned! This is your one and only warning!
-                             
-                             ***Scripted by MMay1818***');
+    	message.channel.send('@everyone This chat is now closed. Talking ***WILL*** get you banned! This is your one and only warning! ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === '!open') {
-    	message.channel.send('@everyone The chat is now open. You may now talk freely!
-                             
-                             ***Scripted by MMay1818***');
+    	message.channel.send('@everyone The chat is now open. You may now talk freely! ***Scripted by MMay1818***');
   	} 
     });
         client.on('message', message => {
     if (message.content === '!trainings') {
-    	message.reply('I'm sorry. The Henry Theatre Training Center is not completed yet. DM MMay1818 for more information.
-                      
-                      ***Scripted by MMay1818***');
+    	message.reply('Im sorry. The Henry Theatre Training Center is not completed yet. DM MMay1818 for more information. ***Scripted by MMay1818***');
   	}
 });
 client.on('message', message => {
     if (message.content === 'ht') {
-    	message.channel.send('***Henry Theatre Assistant**
-                             Henry Theatre assistant is an user-friendly bot scripted by MMay1818. @MR @HR @SHR staff can use this bot to host sessions. Follow the format below:
-                             **THEATRE SHOW:** "!show"
-                             **AUDITIONS:** "!auditions"
-                             **TRAININGS:** "!trainings"
-                             ***Copyright 2019. Henry Theatre Assistant|| Scripted by MMay1818');
+    	message.channel.send('***Henry Theatre Assistant**  Henry Theatre assistant is an user-friendly bot scripted by MMay1818. @MR @HR @SHR staff can use this bot to host sessions. Follow the format below: **THEATRE SHOW:** "!show"  **AUDITIONS:** "!auditions"  **TRAININGS:** "!trainings"  ***Copyright 2019. Henry Theatre Assistant|| Scripted by MMay1818');
   	} 
  });
 client.on('message', message => {
