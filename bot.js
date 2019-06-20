0ed9eaa1fa81d98fc62dd2a52fe428b14b2f72e3
MMay1818Smh <40446817+MMay1818Smh@users.noreply.github.com>
2019-06-20 15:16:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,72 +27,72 @@ client.on('message', message => {
 });
 client.on('message', message => {
     if (message.content === 'fuck') {
-    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server!');
   	}
 });
 client.on('message', message => {
     if (message.content === 'Fuck') {
-    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ');
   	}
 });
 client.on('message', message => {
     if (message.content === 'bitch') {
-    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ');
   	}
 });
 client.on('message', message => {
     if (message.content === 'Bitch') {
-    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ');
   	}
 });
 client.on('message', message => {
     if (message.content === 'cunt') {
-    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ');
   	}
 });
 client.on('message', message => {
     if (message.content === 'Cunt') {
-    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ');
   	}
 });
 client.on('message', message => {
     if (message.content === 'asshole') {
-    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ');
   	}
 });
 client.on('message', message => {
     if (message.content === 'Asshole') {
-    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ');
   	}
 });
 client.on('message', message => {
     if (message.content === 'ass') {
-    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server!');
   	}
 });
 client.on('message', message => {
     if (message.content === 'Ass') {
-    	message.reply('Do not swear in this discord server! ***Scripted by MMay1818***');
+    	message.reply('Do not swear in this discord server! ');
   	}
 });
 client.on('message', message => {
     if (message.content === '!close') {
-    	message.channel.send('@everyone This chat is now closed. Talking ***WILL*** get you banned! This is your one and only warning! ***Scripted by MMay1818***');
+    	message.channel.send('@everyone This chat is now closed. Talking ***WILL*** get you banned! This is your one and only warning! ');
   	}
 });
 client.on('message', message => {
     if (message.content === '!open') {
-    	message.channel.send('@everyone The chat is now open. You may now talk freely! ***Scripted by MMay1818***');
+    	message.channel.send('@everyone The chat is now open. You may now talk freely! ');
   	} 
     });
         client.on('message', message => {
     if (message.content === '!trainings') {
-    	message.reply('Im sorry. The Henry Theatre Training Center is not completed yet. DM MMay1818 for more information. ***Scripted by MMay1818***');
+    	message.reply('Im sorry. The Henry Theatre Training Center is not completed yet. DM MMay1818 for more information. ');
   	}
 });
 client.on('message', message => {
     if (message.content === 'ht') {
-    	message.channel.send('***Henry Theatre Assistant**  Henry Theatre assistant is an user-friendly bot scripted by MMay1818. @MR @HR @SHR staff can use this bot to host sessions. Follow the format below: **THEATRE SHOW:** "!show"  **AUDITIONS:** "!auditions"  **TRAININGS:** "!trainings"  ***Copyright 2019. Henry Theatre Assistant|| Scripted by MMay1818');
+    	message.channel.send('');
   	} 
  });
 client.on('message', message => {
