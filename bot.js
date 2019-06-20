faf71281ce250bb2b9ad28dd84b7b000fa1ee7c8
MMay1818Smh <40446817+MMay1818Smh@users.noreply.github.com>
2019-06-20 14:53:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === '!shift') {
     	message.channel.send({embed: {
-  color: 3447003,
+  color: 5757,
   description: "A very simple Embed!"
 }});;
   	}
