8cc2b4c83ccafb5e7a88df2d069805d1018fbaeb
MMay1818Smh <40446817+MMay1818Smh@users.noreply.github.com>
2019-06-20 15:00:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === '!shift') {
     	message.channel.send({embed: {
-  color: 005757,
+  color: 000000,
   description: "@everyone test"
 }});;
   	}
