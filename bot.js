d9d0b98cc21bb1d874f6b110bd35461bc2aec4e5
MMay1818Smh <40446817+MMay1818Smh@users.noreply.github.com>
2019-06-20 17:09:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,20 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
+client.on("message", (message) => {
+    if (message.content.startsWith("!kick")) {
+        // Easy way to get member object though mentions.
+        var member= message.mentions.members.first();
+        // Kick
+        member.kick().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+        }).catch(() => {
+             // Failmessage
+            message.channel.send("Access Denied");
+        });
+    }
+});
 
 client.on('message', message => {
     if (message.content === '-shift') {
